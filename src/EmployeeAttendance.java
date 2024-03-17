2e2cfea93f2e1e66fcb9b4127c15ea2de91ea9cd
Aditi Patil <aditipatil0506@gmail.com>
2024-03-17 22:50:03 +0530

    initial commit

--- src/EmployeeAttendance.java
+++ src/EmployeeAttendance.java
@@ -1,11 +1,18 @@
-class EmployeeWages {
+class EmployeeWagehour {
     public static void main(String args[]){
-        final int  WAGE_PER_HOUR =20;
-        final int  WAGE_DAY_HOUR =8;
-        int dailyWage = WAGE_PER_HOUR * WAGE_DAY_HOUR;
-        System.out.println("Wage Per Hour =" +WAGE_PER_HOUR);
-        System.out.println("Full Day Hour =" +WAGE_DAY_HOUR);
-        System.out.println("Emlloyee daily Wage =" +dailyWage);
+        int IS_FULL_TIME =1;
+        int EMP_RATE_PER_HOURS =20;
+        int empHrs =0;
+        int empWage =0;
+        double empcheck = Math.floor(Math.random () *10) %2;
+        if(empcheck ==IS_FULL_TIME)
+            empHrs=8;
+        else
+            empHrs =0;
+        empWage = empHrs *EMP_RATE_PER_HOURS;
+        System.out.println("Emp Wage :" +empWage);
+
+
+
     }
 }
-
