6e1426e02ea215d41ac09e869fc83d77c3d085c6
Aditi Patil <aditipatil0506@gmail.com>
2024-03-17 23:09:17 +0530

    initial commit

--- src/EmployeeAttendance.java
+++ src/EmployeeAttendance.java
@@ -1,31 +1,15 @@
-class EmpWagesHrpermonth {
-    public static final int IS_PART_TIME =1;
-    public static final int IS_FULL_TIME =2;
-    public static final int EMP_RATE_PER_HOUR =20;
-    public static final int NUM_OF_WORKING_DAYS =10;
-    public static final int MAX_HR_IN_MONTH= 2;
-    public static void main (String args[]){
-        int empHrs=0, totalEmpHrs =0, totalWorkingDays=0;
-        while(totalEmpHrs <= MAX_HR_IN_MONTH  &&
-                totalWorkingDays<NUM_OF_WORKING_DAYS)
-        {
-            totalWorkingDays++;
-            int empCheck = (int) Math.floor (Math.random() *10)%3;
-            switch(empCheck){
-                case IS_PART_TIME :
-                    empHrs =4;
-                    break;
-                case IS_FULL_TIME :
-                    empHrs=8;
-                    break;
-                default:
-                    empHrs =0;
-            }
-            totalEmpHrs += empHrs;
-            System.out.println("Days:" +totalWorkingDays+ "Emp Hrs:" +empHrs);
-        }
-        int totalEmpWages = empHrs* EMP_RATE_PER_HOUR ;
-        System.out.println("Emp Wages :" +totalEmpWages);
+class Employee {
+    private static double hourlyRate = 10;
 
+
+    public static double computeWage(int hoursWorked) {
+        return hourlyRate * hoursWorked;
+    }
+
+
+    public static void main(String[] args) {
+        int hoursWorked = 40;
+        double wage = Employee.computeWage(hoursWorked);
+        System.out.println("Employee's wage for " + hoursWorked + " hours worked: " + wage);
     }
 }
